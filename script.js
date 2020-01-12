989005937c50bd25336d0da138d91e69ede744e0
Onur <onursazak94@gmail.com>
2020-01-13 00:08:51 +0200

    Class and subclassess

--- script.js
+++ script.js
@@ -7,7 +7,6 @@
     let b = 2;
 }
 
-
 //ES5
 (function() {
 
@@ -15,9 +14,7 @@
 
 */
 
-
 // ES6 LECTURE : STRINGS
-
 /*
 let firstName = 'John';
 let lastName = 'Smith';
@@ -30,7 +27,6 @@ function calcAge(year) {
 // ES5
 console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' +
 calcAge(yearOfBirth) + 'years old. ');
-
 
 // `` =  Alt + 96
 console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
@@ -45,8 +41,6 @@ console.log(firstName.repeat(5)); // prints john 5 times.
 // LECTURE : ARROW FUNCTIONS
 
 /*const years = [1990, 1965, 1982, 1937];
-
-
 
 var ages5 = years.map(function (el) {
     return 2016 - el;
@@ -213,6 +207,7 @@ console.log(ages.find(cur => cur >= 18)
 */
 
 // Lecture: Spread Operator
+/*
 
 function addFourAges(a, b, c, d) {
     return a+b+c+d;
@@ -232,6 +227,7 @@ console.log(sum2);
 const sum3 = addFourAges(...ages);
 console.log(sum3);
 
+
 const familySmith = ['John', 'Jane', 'Mark'];
 const familyMiller = ['Mary', 'Bob', 'Ann'];
 const bigFamily = [...familySmith,...familyMiller];
@@ -240,9 +236,207 @@ const anotherElementinBigFamily = [...familySmith,'Lily',...familyMiller];
 console.log(bigFamily);
 console.log(anotherElementinBigFamily);
 
+*/
+//ES5 way of concatanating 2 array.
+/*
+var array = ['a', 'b'];
+var elements = [0, 1, 2];
+array.push.apply(array,elements);
+console.log("concatenation of 2 array : " + array);
+*/
+
+/*
 const h = document.querySelector('h1');
 const boxes = document.querySelectorAll('.box');
 const all = [h, ...boxes];
 console.log("all : " + Array.from(all));
-// NodeList to array
 all.forEach(cur => cur.style.color = 'purple');
+ */
+
+// Lecture : REST PARAMETERS
+//ES5
+/*
+function isfullAge5() {
+    var argsArr = Array.prototype.slice.call(arguments);
+    argsArr.forEach(function(cur) {
+        console.log((2016 - cur) >= 18);
+    });
+}
+//isfullAge5(1990, 1999, 1965);
+*/
+
+/*
+//ES6
+function isFullAge6(...years) {
+    years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+isFullAge6(1990, 1999, 1965);
+ */
+
+// Lecture: Default Parameters
+//ES5
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'American' : nationality = nationality;
+
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('emily', 1983, 'Diaz', 'Spanish');
+*/
+
+//ES6
+/*
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('emily', 1983, 'Diaz', 'Spanish');
+
+
+//Lecture: Maps
+
+/*
+const question = new Map();
+question.set('question','What is the official name of the latest major JavaScript version ? ');
+question.set(1,'ES5');
+question.set(2,'ES6');
+question.set(3,'ES2015');
+question.set(4,'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer');
+question.set(false, 'Wrong, please try again');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+if(question.has(4)) {
+    //question.delete(4);
+    console.log('answer 4 is here');
+}
+
+//question.clear(); // clear all the elements of question map.
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it is set to ${value}`));
+//console.log(question.entries());
+
+for(let [key, value] of question.entries()) {
+    //console.log(`This is ${key}, and it is set to ${value}`);
+    if(typeof (key) === 'number') {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
+ */
+
+//Lecture : Classes
+
+//ES5
+/*
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+//ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    // calculateAge is a function. we dont need function keyword here.
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+    static greeting() {
+        console.log('Hey there');
+    }
+}
+const john6 = new Person6('John', 1990, 'teacher');
+john6.calculateAge();
+john5.calculateAge();
+Person6.greeting();
+*/
+
+// Lecture: Classes and Subclasses
+//ES5
+/*
+var Person5 = function (name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+}
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+    Person5.call(this, name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+ */
+// ES6
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    // calculateAge is a function. we dont need function keyword here.
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+    static greeting() {
+        console.log('Hey there');
+    }
+}
+class Athlete6 extends Person6 {
+   constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
